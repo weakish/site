1659e95d69af23c8d9e726feb79bde71852235b2
Hong Jiang <j@1byte.io>
2019-03-31 14:25:25 +0800

    feat: add LeanCloud link in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import footerStyles from './footer.module.css';
+import React from 'react'
+import Link from 'gatsby-link'
+import footerStyles from './footer.module.css'
 
 const Footer = () => (
   <div className={footerStyles.footer}>
-    <p>
-      &copy; nextfe.com 2018 - 2019. All rights reserved.
-    </p>
     <nav>
       <ul>
         <li>
-          <Link to='/'>首页</Link>
+          <Link to="/">首页</Link>
         </li>
         <li>
-          <a href='mailto:newfrontendweekly0@163.com'>联系我们</a>
+          <a href="mailto:newfrontendweekly0@163.com">联系我们</a>
         </li>
       </ul>
     </nav>
+    <p>
+      由加速开发的后端云服务 <a href="https://leancloud.cn">LeanCloud</a> 赞助。
+    </p>
+    <p>&copy; nextfe.com 2018 - 2019. All rights reserved.</p>
   </div>
-);
+)
 
-export default Footer;
+export default Footer
